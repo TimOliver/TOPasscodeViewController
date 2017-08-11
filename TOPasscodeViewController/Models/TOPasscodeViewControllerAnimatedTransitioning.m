ee0a754eb5050589f3133bcf7cfbcb81a920eb38
Tim Oliver <me@timoliver.com.au>
2017-08-11 16:38:30 +0800

    Made zoom animation more subtle

--- TOPasscodeViewController/Models/TOPasscodeViewControllerAnimatedTransitioning.m
+++ TOPasscodeViewController/Models/TOPasscodeViewControllerAnimatedTransitioning.m
@@ -1,10 +1,24 @@
 //
 //  TOPasscodeViewControllerAnimatedTransitioning.m
-//  TOPasscodeViewControllerExample
 //
-//  Created by Tim Oliver on 5/15/17.
-//  Copyright © 2017 Timothy Oliver. All rights reserved.
+//  Copyright 2017 Timothy Oliver. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "TOPasscodeViewControllerAnimatedTransitioning.h"
 #import "TOPasscodeViewController.h"
@@ -91,15 +105,13 @@
     if (self.success && self.dismissing) {
         CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform"];
         animation.duration = [self transitionDuration:transitionContext];
-        animation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.75f, 0.75f, 1)];
+        animation.toValue = [NSValue valueWithCATransform3D:CATransform3DMakeScale(0.9f, 0.9f, 1)];
         animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
         [passcodeView.layer addAnimation:animation forKey:@"transform"];
     }
 
     [UIView animateWithDuration:[self transitionDuration:transitionContext]
                           delay:0.0f
-//         usingSpringWithDamping:1.0f
-//          initialSpringVelocity:0.3f
                         options:UIViewAnimationOptionAllowUserInteraction
                      animations:animationBlock
                      completion:completedBlock];
